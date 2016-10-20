e6351ce0e69f6a34995c9f9f283716d4b4a824a0
Krisztian Godrei <godreikrisztian@gmail.com>
2016-10-20 11:15:57 +0200

    go-toolkit, fallback url

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/bitrise-io/go-utils/cmdex"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+// ConfigsModel ...
+type ConfigsModel struct {
+	XamarinSolution string
+	NugetVersion    string
+}
+
+func createConfigsModelFromEnvs() ConfigsModel {
+	return ConfigsModel{
+		XamarinSolution: os.Getenv("xamarin_solution"),
+		NugetVersion:    os.Getenv("nuget_version"),
+	}
+}
+
+func (configs ConfigsModel) print() {
+	log.Info("Configs:")
+
+	log.Detail("- XamarinSolution: %s", configs.XamarinSolution)
+	log.Detail("- NugetVersion: %s", configs.NugetVersion)
+}
+
+func (configs ConfigsModel) validate() error {
+	if configs.XamarinSolution == "" {
+		return errors.New("No XamarinSolution parameter specified!")
+	}
+	if exist, err := pathutil.IsPathExists(configs.XamarinSolution); err != nil {
+		return fmt.Errorf("Failed to check if XamarinSolution exist at: %s, error: %s", configs.XamarinSolution, err)
+	} else if !exist {
+		return fmt.Errorf("XamarinSolution not exist at: %s", configs.XamarinSolution)
+	}
+
+	return nil
+}
+
+// DownloadFile ...
+func DownloadFile(downloadURL, targetPath string) error {
+	outFile, err := os.Create(targetPath)
+	defer func() {
+		if err := outFile.Close(); err != nil {
+			log.Warn("Failed to close (%s)", targetPath)
+		}
+	}()
+	if err != nil {
+		return fmt.Errorf("failed to create (%s), error: %s", targetPath, err)
+	}
+
+	resp, err := http.Get(downloadURL)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Warn("failed to close (%s) body", downloadURL)
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("non success status code: %d", resp.StatusCode)
+	}
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+
+	return nil
+}
+
+func main() {
+	configs := createConfigsModelFromEnvs()
+
+	fmt.Println()
+	configs.print()
+
+	if err := configs.validate(); err != nil {
+		log.Error("Issue with input: %s", err)
+		os.Exit(1)
+	}
+
+	nugetPth := "/Library/Frameworks/Mono.framework/Versions/Current/bin/nuget"
+	nugetRestoreCmdArgs := []string{nugetPth}
+
+	if configs.NugetVersion == "latest" {
+		fmt.Println()
+		log.Info("Updating Nuget to latest version...")
+		// "sudo $nuget update -self"
+		cmdArgs := []string{"sudo", nugetPth, "update", "-self"}
+		cmd, err := cmdex.NewCommandFromSlice(cmdArgs)
+		if err != nil {
+			log.Error("Failed to create command from args (%v), error: %s", cmdArgs, err)
+			os.Exit(1)
+		}
+
+		cmd.SetStdout(os.Stdout)
+		cmd.SetStderr(os.Stderr)
+
+		log.Done("$ %s", cmdex.PrintableCommandArgs(false, cmdArgs))
+
+		if err := cmd.Run(); err != nil {
+			log.Error("Failed to update nuget, error: %s", err)
+			os.Exit(1)
+		}
+	} else if configs.NugetVersion != "" {
+		fmt.Println()
+		log.Info("Downloading Nuget %s version...", configs.NugetVersion)
+		tmpDir, err := pathutil.NormalizedOSTempDirPath("__nuget__")
+		if err != nil {
+			log.Error("Failed to create tmp dir, error: %s", err)
+			os.Exit(1)
+		}
+
+		downloadPth := filepath.Join(tmpDir, "nuget.exe")
+
+		// https://dist.nuget.org/win-x86-commandline/v3.3.0/nuget.exe
+		nugetURL := fmt.Sprintf("https://dist.nuget.org/win-x86-commandline/v%s/nuget.exe", configs.NugetVersion)
+
+		log.Detail("Download URL: %s", nugetURL)
+
+		if err := DownloadFile(nugetURL, downloadPth); err != nil {
+			log.Warn("Download failed, error: %s", err)
+
+			// https://dist.nuget.org/win-x86-commandline/v3.4.4/NuGet.exe
+			nugetURL = fmt.Sprintf("https://dist.nuget.org/win-x86-commandline/v%s/NuGet.exe", configs.NugetVersion)
+
+			log.Detail("Retry download URl: %s", nugetURL)
+
+			if err := DownloadFile(nugetURL, downloadPth); err != nil {
+				log.Error("Failed to download nuget, error: %s", err)
+				os.Exit(1)
+			}
+		}
+
+		nugetRestoreCmdArgs = []string{"mono", downloadPth}
+	}
+
+	fmt.Println()
+	log.Info("Restureing Nuget packages...")
+
+	nugetRestoreCmdArgs = append(nugetRestoreCmdArgs, "restore", configs.XamarinSolution)
+	log.Done("$ %s", cmdex.PrintableCommandArgs(false, nugetRestoreCmdArgs))
+
+	cmd, err := cmdex.NewCommandFromSlice(nugetRestoreCmdArgs)
+	if err != nil {
+		log.Error("Failed to create Nuget command, error: %s", err)
+		os.Exit(1)
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Error("Nuget restore failed, error: %s", err)
+		os.Exit(1)
+	}
+}
--- step.sh
+++ step.sh
@@ -1,98 +1,15 @@
 #!/bin/bash
+set -ex
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-set -e
+tmp_gopath_dir="$(mktemp -d)"
 
-#=======================================
-# Functions
-#=======================================
+go_package_name="github.com/bitrise-io/steps-nuget-restore"
+full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
+mkdir -p "${full_package_path}"
 
-RESTORE='\033[0m'
-RED='\033[00;31m'
-YELLOW='\033[00;33m'
-BLUE='\033[00;34m'
-GREEN='\033[00;32m'
+rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
 
-function color_echo {
-	color=$1
-	msg=$2
-	echo -e "${color}${msg}${RESTORE}"
-}
-
-function echo_fail {
-	msg=$1
-	echo
-	color_echo "${RED}" "${msg}"
-	exit 1
-}
-
-function echo_warn {
-	msg=$1
-	color_echo "${YELLOW}" "${msg}"
-}
-
-function echo_info {
-	msg=$1
-	echo
-	color_echo "${BLUE}" "${msg}"
-}
-
-function echo_details {
-	msg=$1
-	echo "  ${msg}"
-}
-
-function echo_done {
-	msg=$1
-	color_echo "${GREEN}" "  ${msg}"
-}
-
-function validate_required_file_input {
-	key=$1
-	value=$2
-	if [ -z "${value}" ] ; then
-		echo_fail "[!] Missing required input: ${key}"
-	fi
-
-	if [[ ! -e "${value}" ]] ; then
-		echo_fail "[!] File not exist at: ${value}"
-	fi
-}
-
-#=======================================
-# Main
-#=======================================
-
-# Parameters
-echo_info "Configs:"
-echo_details "* xamarin_solution: $xamarin_solution"
-echo_details "* nuget_version: $nuget_version"
-
-validate_required_file_input "xamarin_solution" $xamarin_solution
-
-# Current nuget version
-nuget="/Library/Frameworks/Mono.framework/Versions/Current/bin/nuget"
-
-# Install NuGet version
-if [[ -n "$nuget_version" ]] ; then
-	echo_info "Downloading NuGet version: $nuget_version"
-
-	if [[ "$nuget_version" == "latest" ]] ; then
-		echo_details "sudo $nuget update -self"
-		sudo "$nuget" update -self
-	else
-		nuget_url="https://dist.nuget.org/win-x86-commandline/v$nuget_version/nuget.exe"
-		temp_path=$(mktemp -d)
-		nuget_path="$temp_path/nuget.exe"
-
-		echo_details "curl $nuget_url -o $nuget_path -s"
-		curl "$nuget_url" -o "$nuget_path" -s
-
-		nuget="mono $nuget_path"
-	fi
-fi
-
-# NuGet restore
-echo_info "NuGet restore"
-
-echo_details "$nuget restore $xamarin_solution"
-$nuget restore "$xamarin_solution"
+export GOPATH="${tmp_gopath_dir}"
+export GO15VENDOREXPERIMENT=1
+go run "${full_package_path}/main.go"
